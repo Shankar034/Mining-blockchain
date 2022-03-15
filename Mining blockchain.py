3cbabb4192547d91a53b68ea771165c61fd43275
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-03-15 21:35:36 +0545

    Add files via upload

---
+++ Mining blockchain.py
@@ -0,0 +1,49 @@
+# Part 2 - Mining our blockchain
+
+# Creating a Web App
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof= blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block =  blockchain.create_block(proof, previous_hash)
+    response = {'message': 'congatulations, you just mined a block',
+                'index':block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']
+                
+                }
+    return jsonify(response), 200
+
+#Getting the full Blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+#Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Shankar, we have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+
+#Running the app
+app.run(host = '0.0.0.0 ', port = 5000)
+
